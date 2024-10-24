4e51067f2c845c27d5810a9aaf645eed76f82a51
Ryan Noort <ryannoort@hotmail.com>
2024-10-23 21:45:34 -0600

    fix brakeman

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :commodity_ownerships, dependent: :destroy
   has_many :commodity_transactions, dependent: :destroy
 
-  validates :email_address, presence: true, uniqueness: true, format: { with: /@/ }
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
