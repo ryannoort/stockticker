556db99c00e1ff26c16b11423dc275c57ce705f1
Ryan Noort <ryannoort@hotmail.com>
2024-12-17 16:08:38 -0700

    oops

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,5 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
-ENV["PORT"] ||= "8000"
+ENV["PORT"] ||= "3000"
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
