6345521ed88c9f4546222cd7c51e57dd11d2a2fc
Ryan Noort <ryannoort@hotmail.com>
2024-12-16 20:33:26 -0700

    add stock splits

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -4,15 +4,18 @@ class Commodity < ApplicationRecord
   has_many :commodity_adjustments, dependent: :destroy
 
   def adjust_price(price_change)
-    transaction do
+    transaction(lock: true) do
       self.price += price_change
       commodity_adjustments.create!(price_change: price_change)
 
       if self.price <= 0
-        Rails.logger.info "Price of #{name} is negative"
-        commodity_ownerships.each do |ownership|
-          ownership.destroy!
-        end
+        Rails.logger.info "Price of #{name} has dropped to zero. Destroying all commodity_ownerships"
+        commodity_ownerships.destroy_all
+
+        self.price = 100
+      elsif self.price >= 200
+        Rails.logger.info "Price of #{name} is above 200. Splitting the stocks"
+        commodity_ownerships.update_all("quantity = quantity * 2")
 
         self.price = 100
       end
